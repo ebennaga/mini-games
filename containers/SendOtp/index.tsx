b667835d3e3fb433b4cea0d2322d3605c95f4f6e
rintosaputro <saputrorinto@gmail.com>
2022-09-21 18:05:21 +0700

    add send otp page

---
+++ containers/SendOtp/index.tsx
@@ -0,0 +1,15 @@
+import { Typography } from '@mui/material';
+import React from 'react';
+
+const SendOtp = () => {
+    return (
+        <>
+            <img src='/icons/sending-otp.svg' width={201} height={201} alt='icon sending otp' />
+            <Typography variant='subtitle1' component='p'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.s
+            </Typography>
+        </>
+    );
+};
+
+export default SendOtp;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
+/* eslint-disable no-unused-vars */
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from '@mui/material/styles';
+import { GlobalStyles } from '@mui/material';
 import theme from 'utils/muiTheme';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <ThemeProvider theme={theme}>
+            <GlobalStyles
+                styles={{
+                    // body: { margin: '0 auto', display: 'flex', justifyContent: 'center' }
+                    body: { margin: '0 auto' }
+                }}
+            />
             <Component {...pageProps} />
         </ThemeProvider>
     );
