5478344fade28dc34c7f8d3b67fdcacc752533aa
faryadicka <faryadicka98@gmail.com>
2022-09-21 17:56:29 +0700

    add event onClick an stuff

--- components/Layout/Index.tsx
+++ components/Layout/Index.tsx
@@ -3,24 +3,27 @@ import { Box } from '@mui/material';
 
 interface LayoutProps {
     children: any;
+    backgoundColor: any;
 }
 
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+const Layout: React.FC<LayoutProps> = ({ children, backgoundColor }) => {
     return (
-        <Box
-            sx={{
-                backgroundColor: '#A54CE5',
-                maxWidth: '600px',
-                width: '600px',
-                paddingTop: '80px',
-                paddingBottom: '80px',
-                display: 'flex',
-                justifyContent: 'center',
-                flexDirection: 'column',
-                alignItems: 'center'
-            }}
-        >
-            {children}
+        <Box sx={{ backgroundColor: '#FFF', display: 'flex', justifyContent: 'center' }}>
+            <Box
+                sx={{
+                    backgroundColor: backgoundColor,
+                    maxWidth: '600px',
+                    width: '600px',
+                    paddingTop: '80px',
+                    paddingBottom: '80px',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    flexDirection: 'column',
+                    alignItems: 'center'
+                }}
+            >
+                {children}
+            </Box>
         </Box>
     );
 };
---
+++ containers/SignUp/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Typography, Box } from '@mui/material';
+import Layout from 'components/Layout/Index';
+
+const SignUp = () => {
+    return (
+        <Layout backgoundColor='#FFF'>
+            <Box>
+                <Typography>Start Your Account. It’s Free !</Typography>
+            </Box>
+        </Layout>
+    );
+};
+
+export default SignUp;
