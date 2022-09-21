8ed8cace4411f50b8af6a880efe35baa1c75c39a
rintosaputro <saputrorinto@gmail.com>
2022-09-21 14:55:26 +0700

    config eslint

--- next.config.js
+++ next.config.js
@@ -1,7 +1,30 @@
+/* eslint-disable */
+const EsLintPlugin = require('eslint-webpack-plugin');
+const path = require('path');
+const fs = require('fs');
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+    // reactStrictMode: true,
+    // swcMinify: true,
+    reactStrictMode: false,
+    optimizeFonts: false,
+    webpack: (config, { dev }) => {
+      if (dev) {
+        config.plugins.push(
+          new EsLintPlugin({
+            context: './',
+            extensions: ['js', 'jsx', 'ts', 'tsx'],
+            exclude: ['node_modules', 'dist'],
+            resolvePluginsRelativeTo: __dirname,
+            cacheLocation: path.resolve(resolveApp('node_modules'), '.cache/.eslintcache')
+        })
+        )
+      }
+      return config;
+    }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
