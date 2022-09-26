1dc209b70e91fd0d496c76bbdea2cdc0e2945105
ebennaga <freddy.eben@gmail.com>
2022-09-26 11:49:58 +0700

    component transaction and history

--- containers/History/index.tsx
+++ containers/History/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Typography, Box, ButtonBase } from '@mui/material';
+import { Grid, ButtonBase, Typography, Box } from '@mui/material';
 import { ArrowCircleLeft } from '@mui/icons-material';
 
 import useStyles from './useStyle';
@@ -7,81 +7,188 @@ import useStyles from './useStyle';
 const History = () => {
     const classes = useStyles();
     // eslint-disable-next-line no-unused-vars
-    const cards = [
+    const cardOnGoing = [
         {
-            id: 1,
+            id: 11,
             image: '/images/dingdong.png'
         },
         {
-            id: 2,
+            id: 12,
             image: '/images/dingdong.png'
         }
     ];
+    const cards = [
+        {
+            id: 1,
+            title: 'Today',
+            image: '/images/dingdong.png',
+            price: '1000'
+        },
+        {
+            id: 2,
+            title: '23 Agustus 2022',
+            image: '/images/dingdong.png',
+            price: '5000'
+        },
+        {
+            id: 3,
+            title: '21 September 2022',
+            image: '/images/dingdong.png',
+            price: '3000'
+        },
+        {
+            id: 4,
+            title: '15 September 2022',
+            image: '/images/dingdong.png',
+            price: '5000'
+        }
+    ];
     return (
-        <Box sx={{ color: '#373737', width: '100%' }}>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
-                <ButtonBase sx={{ ml: 5, mt: 5 }}>
-                    <ArrowCircleLeft sx={{ width: '35px', height: '35px', color: '#A54CE5' }} />
-                </ButtonBase>
-                <Typography className={classes.header} sx={{ fontWeight: 'bold', fontSize: '19.5px', mt: 6 }}>
-                    History
-                </Typography>
-            </Box>
-
-            <Box className={classes.pointOngoing} sx={{ marginTop: '30px' }}>
-                <Typography variant='h6' fontWeight='bold' component='h2'>
-                    On Going
-                </Typography>
-            </Box>
-
-            <Box
-                className={classes.pointOngoing}
-                sx={{ position: 'absolute', width: '10px', height: '10px', background: '#A54CE5', mt: '13%' }}
-            />
-            <Box className={classes.pointNode} sx={{ mt: 3, width: '88px', height: '78px', background: '#D9D9D9', borderRadius: '8px' }}>
-                <img className={classes.pointImage} src='/images/dingdong.png' width={88} height={78} alt='maskot-logo' />
-            </Box>
-            <Box className={classes.pointPlace}>
-                <Typography variant='h6' fontWeight='bold' component='h2' sx={{ fontSize: '14px', fontWeight: 700 }}>
-                    Your Place
-                </Typography>
-                <Typography variant='h6' fontWeight='bold' component='h2' sx={{ fontSize: '14px', fontWeight: 300 }}>
-                    Tournament
-                </Typography>
-            </Box>
-            <Box className={classes.pointOngoing} sx={{ marginTop: '38px' }}>
-                <Typography variant='h6' fontWeight='bold' component='h2'>
-                    Today
-                </Typography>
-            </Box>
-            <Box className={classes.pointNode} sx={{ mt: 3, width: '88px', height: '78px', background: '#D9D9D9', borderRadius: '8px' }}>
-                <img className={classes.pointImage} src='/images/dingdong.png' width={88} height={78} alt='maskot-logo' />
-            </Box>
-            <Box className={classes.pointPlace}>
-                <Typography variant='h6' fontWeight='bold' component='h2' sx={{ fontSize: '14px', fontWeight: 700 }}>
-                    Tournament End
-                </Typography>
-                <Typography variant='h6' fontWeight='bold' component='h2' sx={{ fontSize: '14px', fontWeight: 300 }}>
-                    Games 10.03
-                </Typography>
-            </Box>
-            <Box className={classes.pointOngoing} sx={{ marginTop: '38px' }}>
-                <Typography variant='h6' fontWeight='bold' component='h2'>
-                    23 Agustus 2022
-                </Typography>
-            </Box>
-            <Box className={classes.pointNode} sx={{ mt: 3, width: '88px', height: '78px', background: '#D9D9D9', borderRadius: '8px' }}>
-                <img className={classes.pointImage} src='/images/dingdong.png' width={88} height={78} alt='maskot-logo' />
-            </Box>
-            <Box className={classes.pointPlace}>
-                <Typography variant='h6' fontWeight='bold' component='h2' sx={{ fontSize: '14px', fontWeight: 700 }}>
-                    Participate Tournament
-                </Typography>
-                <Typography variant='h6' fontWeight='bold' component='h2' sx={{ fontSize: '14px', fontWeight: 300 }}>
-                    Games 10.03
-                </Typography>
-            </Box>
-        </Box>
+        <Grid container spacing={2} sx={{ ml: 1 }} direction='column' alignItems='space-between'>
+            <Grid item xs={4}>
+                <Box>
+                    <ButtonBase sx={{ mt: 5 }}>
+                        <ArrowCircleLeft sx={{ width: '35px', height: '35px', color: '#A54CE5' }} />
+                    </ButtonBase>
+                </Box>
+                <Box sx={{ mt: -10, ml: 20 }}>
+                    <Typography sx={{ fontWeight: 'bold', fontSize: '19.5px', mt: 6 }}>History</Typography>
+                </Box>
+            </Grid>
+            <Grid item xs={4}>
+                <Typography sx={{ fontWeight: 'bold', fontSize: '19.5px', mt: 2 }}>On Going</Typography>
+            </Grid>
+            {cardOnGoing.map((index: any) => {
+                return (
+                    <Grid item xs={4}>
+                        <Box
+                            key={index.id}
+                            sx={{
+                                mt: 2,
+                                width: '88px',
+                                height: '78px',
+                                background: '#D9D9D9',
+                                borderRadius: '8px',
+                                display: 'flex',
+                                justifyContent: 'space-between',
+                                ml: 2
+                            }}
+                        >
+                            <img src='/images/dingdong.png' width={88} height={78} alt='maskot-logo' style={{ borderRadius: '12px' }} />
+                            <Box sx={{ ml: 2 }}>
+                                <Box>
+                                    <Typography variant='h6' fontWeight='bold' component='h2' sx={{ fontSize: '14px', fontWeight: 700 }}>
+                                        Tournament
+                                    </Typography>
+                                </Box>
+                                <Box sx={{ mt: 2 }}>
+                                    <Typography
+                                        variant='h6'
+                                        fontWeight='bold'
+                                        component='h2'
+                                        sx={{ fontSize: '14px', fontWeight: 300, lineHeight: '14px' }}
+                                    >
+                                        Games 10.03
+                                    </Typography>
+                                </Box>
+                            </Box>
+                            <Box sx={{ ml: 6, width: '100%', mt: 2 }}>
+                                <ButtonBase sx={{ background: '#A54CE5', borderRadius: '12px', width: '94px', height: '40px' }}>
+                                    <Typography
+                                        variant='h6'
+                                        fontWeight='bold'
+                                        component='h2'
+                                        sx={{ fontSize: '14px', fontWeight: 700, color: '#ffffff' }}
+                                    >
+                                        Go
+                                    </Typography>
+                                </ButtonBase>
+                            </Box>
+                        </Box>
+                        <Grid item xs={1}>
+                            <Box sx={{ border: '1px solid rgba(40, 38, 38, 0.1)', mt: 2, width: '343px', ml: 1, height: '0px' }} />
+                        </Grid>
+                    </Grid>
+                );
+            })}
+            {cards.map((index: any) => {
+                return (
+                    <>
+                        <Grid item xs={4} key={index.id}>
+                            <Typography sx={{ fontWeight: 'bold', fontSize: '19.5px', mt: 2 }}>{index.title}</Typography>
+                        </Grid>
+                        <Grid item xs={4} key={index.id}>
+                            <Box
+                                sx={{
+                                    mt: 2,
+                                    width: '88px',
+                                    height: '78px',
+                                    background: '#D9D9D9',
+                                    borderRadius: '22px',
+                                    display: 'flex',
+                                    justifyContent: 'space-between',
+                                    ml: 2
+                                }}
+                            >
+                                <img
+                                    className={classes.pointImage}
+                                    src='/images/dingdong.png'
+                                    width={88}
+                                    height={78}
+                                    alt='maskot-logo'
+                                    style={{ borderRadius: '12px' }}
+                                />
+                                <Box sx={{ ml: 2 }}>
+                                    <Box>
+                                        <Typography
+                                            variant='h6'
+                                            fontWeight='bold'
+                                            component='h2'
+                                            sx={{ fontSize: '14px', fontWeight: 700 }}
+                                        >
+                                            Tournament
+                                        </Typography>
+                                    </Box>
+                                    <Box sx={{ mt: 2 }}>
+                                        <Typography
+                                            variant='h6'
+                                            fontWeight='bold'
+                                            component='h2'
+                                            sx={{ fontSize: '14px', fontWeight: 300, lineHeight: '14px' }}
+                                        >
+                                            Games 10.03
+                                        </Typography>
+                                    </Box>
+                                </Box>
+                                <Box sx={{ ml: 7, width: '100%' }}>
+                                    <Box>
+                                        <Typography
+                                            variant='h6'
+                                            fontWeight='bold'
+                                            component='h2'
+                                            sx={{ fontSize: '14px', fontWeight: 700 }}
+                                        >
+                                            Place100th
+                                        </Typography>
+                                    </Box>
+                                    <Box sx={{ mt: 2, ml: '25px' }}>
+                                        <Typography
+                                            variant='h6'
+                                            fontWeight='bold'
+                                            component='h2'
+                                            sx={{ fontSize: '14px', fontWeight: 700, color: '#1BA95D' }}
+                                        >
+                                            {index.price} <img src='/images/star_icon.png' width={13} height={13} alt='star-icon' />
+                                        </Typography>
+                                    </Box>
+                                </Box>
+                            </Box>
+                        </Grid>
+                        <Box sx={{ border: '1px solid rgba(40, 38, 38, 0.1)', mt: 2, width: '353px', ml: 2 }} />
+                    </>
+                );
+            })}
+        </Grid>
     );
 };
 
--- containers/History/useStyle.tsx
+++ containers/History/useStyle.tsx
@@ -2,28 +2,11 @@ import { makeStyles } from '@mui/styles';
 
 const useStyles = makeStyles(() => {
     return {
-        header: {
-            display: 'flex',
-            justifyContent: 'center',
-            width: '100%'
-        },
-        headerRight: {
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center'
-        },
-        pointOngoing: {
-            marginLeft: 40
-        },
-        pointNode: {
-            marginLeft: 65
-        },
-        pointPlace: {
-            marginLeft: '45%',
-            marginTop: -60
-        },
         pointImage: {
-            borderRadius: '8px'
+            borderRadius: '13px',
+            '&.makeStyles-pointImage-98': {
+                borderRadius: '12px'
+            }
         }
     };
 });
