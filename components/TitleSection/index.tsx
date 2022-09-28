11e39e577ae21021fa5ed788fcff9eb308c92197
rintosaputro <saputrorinto@gmail.com>
2022-09-28 17:19:24 +0700

    add privacy policy page

---
+++ components/TitleSection/index.tsx
@@ -0,0 +1,34 @@
+import { Box, Typography } from '@mui/material';
+import React from 'react';
+
+interface TitleSectionProps {
+    title: string;
+    sutTitle: string;
+    smallImage: any;
+    bigImage: any;
+}
+
+const TitleSection: React.FC<TitleSectionProps> = ({ title, sutTitle, smallImage, bigImage }) => {
+    return (
+        <Box sx={{ padding: '28px 0', position: 'relative', overflow: 'hidden', display: 'flex' }}>
+            <Box>
+                <Typography fontSize='20px' fontWeight={700} component='h2'>
+                    {title}
+                </Typography>
+                <Typography fontSize='12px' marginTop='16px' fontWeight={600} component='p'>
+                    {sutTitle}
+                </Typography>
+            </Box>
+            <Box sx={{ display: 'flex', position: 'absolute', right: '-28px', top: '3px', rotate: '-33deg' }}>
+                <Box>
+                    <img src={smallImage} width='62.79px' height='59.36px' alt='coins' />
+                </Box>
+                <Box sx={{ marginLeft: '-25px' }}>
+                    <img src={bigImage} width='83.64px' height='70.93px' alt='coins' />
+                </Box>
+            </Box>
+        </Box>
+    );
+};
+
+export default TitleSection;
---
+++ containers/PrivacyPolicy/index.tsx
@@ -0,0 +1,50 @@
+import { Box } from '@mui/material';
+import HeaderBack from 'components/HeaderBack';
+import InfoArticle from 'components/InfoArticle';
+import TitleSection from 'components/TitleSection';
+import React from 'react';
+
+const PrivacyPolicy = () => {
+    return (
+        <Box component='main' sx={{ width: '-webkit-fill-available', padding: '0 20px', color: '#373737' }}>
+            <HeaderBack title='Privacy Policy' />
+            <Box component='section' sx={{ marginTop: '27px' }}>
+                <TitleSection
+                    title='Prize Play Privacy Policy'
+                    sutTitle='Last Updated: Okt 20 2022'
+                    smallImage='/icons/coins.svg'
+                    bigImage='/icons/coins.svg'
+                />
+            </Box>
+            <Box component='article'>
+                {[...Array(2)].map((_item: any, index: number) => {
+                    return (
+                        <Box key={index} sx={{ marginTop: index === 0 ? 0 : '28px' }}>
+                            <InfoArticle
+                                text='Prize play is dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class
+                                aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis
+                                condimentum lobortis.'
+                            />
+                        </Box>
+                    );
+                })}
+                <Box sx={{ marginTop: '28px' }}>
+                    {[...Array(4)].map((_item: any, index: number) => {
+                        return (
+                            <Box key={index} sx={{ marginTop: index === 0 ? 0 : '28px' }}>
+                                <InfoArticle
+                                    title={`${index + 1}. Lorem ipsum dolor sit amet`}
+                                    text='Prize play is dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class
+                                aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis
+                                condimentum lobortis.'
+                                />
+                            </Box>
+                        );
+                    })}
+                </Box>
+            </Box>
+        </Box>
+    );
+};
+
+export default PrivacyPolicy;
