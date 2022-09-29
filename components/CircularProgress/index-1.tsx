133db3b97e96148c5bc7664127841c3ef37ad96c
faryadicka <faryadicka98@gmail.com>
2022-09-29 15:28:52 +0700

    fix layouting loading casual page

---
+++ components/CircularProgress/index-1.tsx
@@ -0,0 +1,81 @@
+/* eslint-disable no-nested-ternary */
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+import CircularProgress, { circularProgressClasses, CircularProgressProps } from '@mui/material/CircularProgress';
+
+function LoadingProgress(props: CircularProgressProps & { value: number }) {
+    const { value } = props;
+    const label = value === 25 ? 0 : value === 50 ? 1 : value === 75 ? 2 : 3;
+    return (
+        <Box sx={{ position: 'relative' }}>
+            <CircularProgress
+                variant='determinate'
+                sx={{
+                    color: (theme) => theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
+                    '& .MuiCircularProgress-circle': {
+                        strokeWidth: 1.5
+                    }
+                }}
+                size={130}
+                thickness={4}
+                {...props}
+                value={100}
+            />
+            <CircularProgress
+                variant='indeterminate'
+                disableShrink
+                sx={{
+                    color: (theme) => (theme.palette.mode === 'light' ? '#A54CE5' : '#308fe8'),
+                    animationDuration: '1000ms',
+                    position: 'absolute',
+                    left: 0,
+                    [`& .${circularProgressClasses.circle}`]: {
+                        strokeLinecap: 'round'
+                    },
+                    '& .MuiCircularProgress-circle': {
+                        strokeWidth: 1.5,
+                        strokeLinecap: 'square'
+                    }
+                }}
+                size={130}
+                thickness={4}
+                value={25}
+                // {...props}
+            />
+            <Box
+                sx={{
+                    top: 0,
+                    left: 0,
+                    bottom: 0,
+                    right: 0,
+                    position: 'absolute',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}
+            >
+                <Typography variant='caption' component='div' sx={{ fontSize: '50px', fontWeight: 'bold' }}>{`${Math.round(
+                    label
+                )}`}</Typography>
+            </Box>
+        </Box>
+    );
+}
+
+export default function CustomizedProgressBars() {
+    const [progress, setProgress] = React.useState(100);
+
+    React.useEffect(() => {
+        const timer = setInterval(() => {
+            setProgress((prevProgress) => (prevProgress <= 0 ? 75 : prevProgress - 25));
+        }, 1000);
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <LoadingProgress value={progress} />
+        </Box>
+    );
+}
---
+++ containers/Casual/index.tsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import { Box, Typography, Grid } from '@mui/material';
+import Header from 'components/Header';
+import Button from 'components/Button/Index';
+import { SmartDisplay, EmojiEvents, Share } from '@mui/icons-material';
+
+const CasualContainer = () => {
+    return (
+        <Box sx={{ width: '100%' }}>
+            {/* dari sini */}
+            <Box
+                sx={{
+                    backgroundImage: `url(${'/images/bg-casual.png'})`,
+                    height: '50vh',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'space-between',
+                    position: 'relative',
+                    zIndex: 0,
+                    // padding: '10px 20px',
+                    borderRadius: '0px 0px 20px 20px'
+                }}
+            >
+                <Box sx={{ padding: '10px 20px', position: 'relative', zIndex: 1 }}>
+                    <Header isBack point={102_300} profilePicture='/icons/dummy/profile.png' />
+                </Box>
+                <Box
+                    sx={{
+                        backgroundImage: 'linear-gradient(transparent, #353535)',
+                        height: '100%',
+                        position: 'relative',
+                        width: '100%',
+                        bottom: '24%'
+                    }}
+                />
+                <Box
+                    sx={{
+                        backgroundColor: '#353535',
+                        width: '100%',
+                        height: '25%',
+                        position: 'absolute',
+                        bottom: '0px',
+                        borderRadius: '0px 0px 20px 20px'
+                    }}
+                >
+                    <Box sx={{ paddingX: '20px', paddingBottom: '20px' }}>
+                        <Typography sx={{ color: 'white', fontSize: '24px', fontWeight: 700 }}>Block Stack</Typography>
+                        <Typography sx={{ color: 'white', fontSize: '18px', fontWeight: 700 }}>Casual Play</Typography>
+                    </Box>
+                </Box>
+            </Box>
+            {/* sampai sini */}
+            <Grid container direction='column' padding='20px'>
+                <Grid item xs={12}>
+                    <Typography sx={{ color: '#949494', fontSize: '14px', fontWeight: 400 }}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                    </Typography>
+                    <Box sx={{ mt: '25px' }}>
+                        <Button title='Free' backgoundColor='#A54CE5' color='white' />
+                    </Box>
+                    <Box display='flex' alignItems='center' justifyContent='center' gap='5px' mt='20px'>
+                        <Typography sx={{ fontWeight: 'bold' }}>Watch Ads</Typography>
+                        <SmartDisplay sx={{ color: '#A54CE5' }} />
+                        <Typography>to Play</Typography>
+                    </Box>
+                </Grid>
+            </Grid>
+            <Grid container padding='10px 20px' mt={3}>
+                <Grid item xs={6}>
+                    <Typography variant='h5' sx={{ fontWeight: 'bold' }}>
+                        Your Stats
+                    </Typography>
+                </Grid>
+                <Grid
+                    mt='18px'
+                    mb='310px'
+                    container
+                    justifyContent='space-between'
+                    xs={12}
+                    gap='10px'
+                    sx={{ backgroundColor: '#F4F1FF', padding: '18px 25px', borderRadius: '15px' }}
+                >
+                    <Grid container xs={5} justifyContent='space-between'>
+                        <img src='/icons/tape-stats.png' alt='ribbon' />
+                        <Typography sx={{ fontSize: '14px', fontWeight: '700' }}>High scores</Typography>
+                    </Grid>
+                    <Grid container xs={5} justifyContent='space-between'>
+                        <EmojiEvents />
+                        <Typography sx={{ fontWeight: '700' }}>211.876</Typography>
+                        <Share />
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Box>
+    );
+};
+
+export default CasualContainer;
