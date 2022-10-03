5f97a8f7ee0760ccba7cac1d8e2c61e3a8d4bb03
rintosaputro <saputrorinto@gmail.com>
2022-10-03 13:39:09 +0700

    add skeleton home

---
+++ components/TournamentSlider/TournamentSliderSkeleton.tsx
@@ -0,0 +1,17 @@
+import { Grid, Skeleton } from '@mui/material';
+import React from 'react';
+
+const TournamentSliderSkeleton = () => {
+    return (
+        <Grid container spacing={2} mb={2}>
+            <Grid item height='330px' xs={10}>
+                <Skeleton sx={{ height: '400px', marginTop: '-40px' }} />
+            </Grid>
+            <Grid item height='330px' xs={2}>
+                <Skeleton sx={{ height: '400px', marginTop: '-40px' }} />
+            </Grid>
+        </Grid>
+    );
+};
+
+export default TournamentSliderSkeleton;
