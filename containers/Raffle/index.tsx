f78d47b7522de4d94929ad8513672073d30c3918
faryadicka <faryadicka98@gmail.com>
2022-10-03 11:22:24 +0700

    fix layouting lucky-raffle pages

--- containers/Raffle/index.tsx
+++ containers/Raffle/index.tsx
@@ -2,7 +2,7 @@
 import { Box, Grid, Typography, ButtonBase } from '@mui/material';
 import React from 'react';
 import Header from 'components/Header';
-import { WatchLater, Add, Remove, ArrowBack, ArrowForward } from '@mui/icons-material';
+import { WatchLater, Add, Remove, ArrowBack, ArrowForward, HelpOutline } from '@mui/icons-material';
 import Button from 'components/Button/Index';
 import StatusRoundDialog from 'components/Dialog/StatusRoundDialog';
 import RewardDialog from 'components/Dialog/RewardDialog';
@@ -14,6 +14,8 @@ const RaffleContainer = () => {
     const [openBuyDialog, setOpenBuyDialog] = React.useState<any>(false);
     const [openRewardDialog, setOpenRewardDialog] = React.useState<any>(false);
     const [openStatusRoundDialog, setOpenStatusRoundDialog] = React.useState<any>(true);
+    const [isWinner, setIsWinner] = React.useState<boolean>(false);
+    const [roundDay, setRoundDay] = React.useState<boolean>(true);
 
     const dataList = [
         { image: '/icons/dummy/profile-2.png', username: 'rinto', tickets: 246000, prize: 2000 },
@@ -38,7 +40,7 @@ const RaffleContainer = () => {
     return (
         <Box sx={{ width: '100%' }}>
             <Box
-                padding='20px'
+                padding='15px 20px'
                 sx={{
                     borderBottom: '1px solid rgba(148, 148, 148, 0.35)',
                     mb: 2,
@@ -51,7 +53,27 @@ const RaffleContainer = () => {
                 <Header isShops logo='/icons/logo.svg' point={102_300} profilePicture='/icons/dummy/profile.png' />
             </Box>
             <Box padding='20px'>
-                <Typography sx={{ fontWeight: 700, fontSize: '24px' }}>Lucky Raffle</Typography>
+                <Grid container sx={{ width: '100%', justifyContent: 'space-between', alignItems: 'center' }}>
+                    <Grid item xs={6}>
+                        <Typography sx={{ fontWeight: 700, fontSize: '24px' }}>Lucky Raffle</Typography>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <ButtonBase
+                            sx={{
+                                display: 'flex',
+                                width: '100%',
+                                backgroundColor: '#949494',
+                                alignItems: 'center',
+                                padding: '3px 1px',
+                                borderRadius: '15px',
+                                justifyContent: 'space-evenly'
+                            }}
+                        >
+                            <Typography sx={{ fontSize: '14px', color: 'white', fontWeight: 600 }}>Tutorials</Typography>
+                            <HelpOutline sx={{ fontSize: '15px', color: 'white' }} />
+                        </ButtonBase>
+                    </Grid>
+                </Grid>
                 <Grid container justifyContent='center' alignItems='center' sx={{ mt: '25px' }}>
                     <Grid
                         container
@@ -93,6 +115,40 @@ const RaffleContainer = () => {
                             <img src='/images/lucky-raffle.png' alt='luckyraffle' />
                         </Grid>
                     </Grid>
+                    <Box sx={{ width: '100%', textAlign: 'center' }}>
+                        <Box
+                            sx={{
+                                backgroundColor: '#FF4567',
+                                mt: '20px',
+                                borderRadius: '8px',
+                                color: 'white',
+                                textAlign: 'center',
+                                paddingTop: '5px'
+                            }}
+                        >
+                            <Typography sx={{ fontSize: '12px', fontWeight: 700 }}>Next Lucky Raffle Round 286 starts in</Typography>
+                            <Box
+                                sx={{ display: 'flex', mb: 2, color: 'white', gap: '5px', alignItems: 'center', justifyContent: 'center' }}
+                            >
+                                <WatchLater sx={{ width: '15px' }} />
+                                <Typography sx={{ fontSize: '12px' }}>6d 13h 23m</Typography>
+                            </Box>
+                        </Box>
+                        <Typography sx={{ fontSize: '24px', color: '#373737', fontWeight: 700 }}>
+                            Winners of Raffle rounds 285 <br />{' '}
+                            {isWinner ? (
+                                <span style={{ color: '#949494', fontSize: '14px' }}>
+                                    The winner is <span style={{ color: '#A54CE5' }}>you</span>
+                                </span>
+                            ) : (
+                                <span style={{ color: '#949494', fontSize: '14px' }}>You aren`t the winner in this round</span>
+                            )}
+                        </Typography>
+                        <Box sx={{ mt: '20px' }}>
+                            <img src={isWinner ? '/images/winner.png' : '/images/not-winner.png'} alt='winner' />
+                            <Typography sx={{ fontSize: '20px', fontWeight: 700, mt: '30px' }}>Arya Stark</Typography>
+                        </Box>
+                    </Box>
                     <Box sx={{ width: '100%' }}>
                         <Grid
                             container
@@ -107,7 +163,7 @@ const RaffleContainer = () => {
                             justifyContent='center'
                         >
                             <Grid item xs={4}>
-                                <Typography sx={{ fontSize: '12px' }}>Your Tickets</Typography>
+                                <Typography sx={{ fontSize: '12px' }}>{roundDay ? 'Round' : 'Your Tickets'}</Typography>
                             </Grid>
                             <Grid
                                 item
@@ -117,10 +173,10 @@ const RaffleContainer = () => {
                                     borderRight: '1px solid rgba(255, 255, 255, 0.05)'
                                 }}
                             >
-                                <Typography sx={{ fontSize: '12px' }}>Total Tickets</Typography>
+                                <Typography sx={{ fontSize: '12px' }}>{roundDay ? 'Prize' : 'Total Tickets'}</Typography>
                             </Grid>
                             <Grid item xs={4}>
-                                <Typography sx={{ fontSize: '12px' }}>Win Chance</Typography>
+                                <Typography sx={{ fontSize: '12px' }}>{roundDay ? 'Your Tickets' : 'Win Chance'}</Typography>
                             </Grid>
                         </Grid>
                         <Grid
@@ -136,7 +192,9 @@ const RaffleContainer = () => {
                             alignItems='center'
                         >
                             <Grid item xs={4}>
-                                <Typography sx={{ fontSize: '20px', fontWeight: 700 }}>10</Typography>
+                                <Typography sx={{ fontSize: roundDay ? '13px' : '20px', fontWeight: 700 }}>
+                                    {roundDay ? '285' : '10'}
+                                </Typography>
                             </Grid>
                             <Grid
                                 item
@@ -146,65 +204,76 @@ const RaffleContainer = () => {
                                     borderRight: '1px solid rgba(40, 38, 38, 0.05)'
                                 }}
                             >
-                                <Typography sx={{ fontSize: '13px', fontWeight: 600 }}>101.195.723</Typography>
+                                {roundDay ? (
+                                    <Box sx={{ display: 'flex', aligItems: 'center', justifyContent: 'center', gap: '2px' }}>
+                                        <Box>
+                                            <img src='/images/point-shops.png' alt='points' />
+                                        </Box>
+                                        <Typography sx={{ fontSize: '13px', fontWeight: 700 }}>350.000</Typography>
+                                    </Box>
+                                ) : (
+                                    <Typography sx={{ fontSize: '13px', fontWeight: 600 }}>101.195.723</Typography>
+                                )}
                             </Grid>
                             <Grid item xs={4}>
-                                <Typography sx={{ fontSize: '13px', fontWeight: 600 }}>0.00001976%</Typography>
+                                <Typography sx={{ fontSize: '13px', fontWeight: 600 }}>{roundDay ? '10' : '0.00001976%'}</Typography>
                             </Grid>
                         </Grid>
                     </Box>
-                    <Grid container justifyContent='center' alignItems='center' sx={{ mt: '50px' }}>
-                        <Grid item xs={6}>
-                            <Typography sx={{ textAlign: 'center', color: '#949494', fontSize: '14px', fontWeight: 500 }}>
-                                No. of Raffle
-                            </Typography>
-                        </Grid>
-                        <Grid container justifyContent='space-between' sx={{ mt: '20px' }} alignItems='center'>
-                            <ButtonBase
-                                disableRipple
-                                onClick={() => {
-                                    if (quantity !== 0) {
-                                        setQuantity(quantity - 1);
-                                    }
-                                }}
-                                sx={{
-                                    ':active': { backgroundColor: '#D9D9D9' },
-                                    cursor: 'pointer',
-                                    padding: '10px',
-                                    border: '1px solid black',
-                                    borderRadius: '100%'
-                                }}
-                            >
-                                <Remove sx={{ fontWeight: 'bold' }} />
-                            </ButtonBase>
-                            <Typography sx={{ fontSize: '50px', fontWeight: 'bold', color: '#373737' }}>{quantity}</Typography>
-                            <ButtonBase
-                                disableRipple
-                                onClick={() => {
-                                    if (quantity >= 0) {
-                                        setQuantity(quantity + 1);
-                                    }
-                                }}
-                                sx={{
-                                    ':active': { backgroundColor: '#D9D9D9' },
-                                    cursor: 'pointer',
-                                    padding: '10px',
-                                    border: '1px solid black',
-                                    borderRadius: '100%'
-                                }}
-                            >
-                                <Add />
-                            </ButtonBase>
-                        </Grid>
-                        <Grid item xs={6} mt={3}>
-                            <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '6px' }}>
-                                <img src='/images/point-shops.png' alt='point' />
-                                <Typography sx={{ textAlign: 'center', fontSize: '14px' }}>
-                                    <span style={{ fontWeight: 'bold' }}>100</span> per Raffle
+                    {!roundDay && (
+                        <Grid container justifyContent='center' alignItems='center' sx={{ mt: '50px' }}>
+                            <Grid item xs={6}>
+                                <Typography sx={{ textAlign: 'center', color: '#949494', fontSize: '14px', fontWeight: 500 }}>
+                                    No. of Raffle
                                 </Typography>
-                            </Box>
+                            </Grid>
+                            <Grid container justifyContent='space-between' sx={{ mt: '20px' }} alignItems='center'>
+                                <ButtonBase
+                                    disableRipple
+                                    onClick={() => {
+                                        if (quantity !== 0) {
+                                            setQuantity(quantity - 1);
+                                        }
+                                    }}
+                                    sx={{
+                                        ':active': { backgroundColor: '#D9D9D9' },
+                                        cursor: 'pointer',
+                                        padding: '10px',
+                                        border: '1px solid black',
+                                        borderRadius: '100%'
+                                    }}
+                                >
+                                    <Remove sx={{ fontWeight: 'bold' }} />
+                                </ButtonBase>
+                                <Typography sx={{ fontSize: '50px', fontWeight: 'bold', color: '#373737' }}>{quantity}</Typography>
+                                <ButtonBase
+                                    disableRipple
+                                    onClick={() => {
+                                        if (quantity >= 0) {
+                                            setQuantity(quantity + 1);
+                                        }
+                                    }}
+                                    sx={{
+                                        ':active': { backgroundColor: '#D9D9D9' },
+                                        cursor: 'pointer',
+                                        padding: '10px',
+                                        border: '1px solid black',
+                                        borderRadius: '100%'
+                                    }}
+                                >
+                                    <Add />
+                                </ButtonBase>
+                            </Grid>
+                            <Grid item xs={6} mt={3}>
+                                <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '6px' }}>
+                                    <img src='/images/point-shops.png' alt='point' />
+                                    <Typography sx={{ textAlign: 'center', fontSize: '14px' }}>
+                                        <span style={{ fontWeight: 'bold' }}>100</span> per Raffle
+                                    </Typography>
+                                </Box>
+                            </Grid>
                         </Grid>
-                    </Grid>
+                    )}
                     <RaffleWinners dataList={dataList} />
                     <Box sx={{ display: 'flex', alignItems: 'center', gap: '30px', my: '30px' }}>
                         <ButtonBase
@@ -233,7 +302,9 @@ const RaffleContainer = () => {
                         </ButtonBase>
                     </Box>
                 </Grid>
-                <Button onClick={() => setOpenBuyDialog(!openBuyDialog)} title='Buy Raffle' backgoundColor='#A54CE5' color='white' />
+                {!roundDay && (
+                    <Button onClick={() => setOpenBuyDialog(!openBuyDialog)} title='Buy Raffle' backgoundColor='#A54CE5' color='white' />
+                )}
             </Box>
             <BuyTicketDialog handleBuyRaffle={handleBuyRaffle} count={quantity} open={openBuyDialog} setOpen={setOpenBuyDialog} />
             <RewardDialog
@@ -241,7 +312,7 @@ const RaffleContainer = () => {
                 open={openRewardDialog}
                 setOpenDialog={setOpenRewardDialog}
             />
-            <StatusRoundDialog open={openStatusRoundDialog} setOpen={setOpenStatusRoundDialog} />
+            <StatusRoundDialog isWinner={isWinner} open={openStatusRoundDialog} setOpen={setOpenStatusRoundDialog} />
         </Box>
     );
 };
