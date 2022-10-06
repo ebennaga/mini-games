3c21d7cb802658c9f0647ce0b6672aba1f90729c
ebennaga <freddy.eben@gmail.com>
2022-10-06 11:22:41 +0700

    add skeleton

--- containers/GameTournament/index.tsx
+++ containers/GameTournament/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Skeleton } from '@mui/material';
 import React from 'react';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import { useRouter } from 'next/router';
@@ -40,7 +40,7 @@ const GameTournament = () => {
                 endpoint: `/tournamets/${id}`,
                 method: 'GET'
             });
-            console.log('res tournament', res);
+
             if (res.status === 200) {
                 setListingGame(res.data.data);
             }
@@ -52,11 +52,10 @@ const GameTournament = () => {
     React.useEffect(() => {
         fetchData(listingGame);
     }, []);
-    console.log('listing game', listingGame);
-    console.log('isLoading', isLoading);
-    if (isLoading || !listingGame) {
-        return <Box>Loading</Box>;
-    }
+
+    // if (isLoading || !listingGame) {
+    //     return <Box>Loading</Box>;
+    // }
     return (
         <Box width='100%'>
             <Header isBack point={myCoins} profilePicture='/icons/dummy/profile-2.png' paddingX='20px' />
@@ -71,18 +70,32 @@ const GameTournament = () => {
                 playerImg3='/icons/dummy/user-2.png'
             />
             <Box component='main' padding='20px' color='#373737'>
-                <Box component='section' padding='28px 0'>
-                    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginBottom: '37px' }}>
-                        <Typography component='h2' fontSize='24px' fontWeight={700}>
-                            Leaderboard
-                        </Typography>
-                        <ArrowForwardIcon sx={{ color: '#373737' }} />
+                {isLoading || !listingGame ? (
+                    <Box>
+                        <Skeleton animation='wave' variant='rectangular' width='100%' />
                     </Box>
-                    <LeaderboardPodium dataLeaderboard={listingGame.leaderboards} />
-                </Box>
-                <Box component='section' marginBottom='40px'>
-                    <TableRank dataLeaderboard={listingGame.leaderboards} />
-                </Box>
+                ) : (
+                    <Box component='section' padding='28px 0'>
+                        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginBottom: '37px' }}>
+                            <Typography component='h2' fontSize='24px' fontWeight={700}>
+                                Leaderboard
+                            </Typography>
+                            <ArrowForwardIcon sx={{ color: '#373737' }} />
+                        </Box>
+                        <LeaderboardPodium dataLeaderboard={listingGame.leaderboards} />
+                    </Box>
+                )}
+                {isLoading || !listingGame ? (
+                    <Box>
+                        {[...Array(10)].map((index: any) => {
+                            return <Skeleton key={index} variant='rectangular' sx={{ my: 4, mx: 1 }} />;
+                        })}
+                    </Box>
+                ) : (
+                    <Box component='section' marginBottom='40px'>
+                        <TableRank dataLeaderboard={listingGame.leaderboards} />
+                    </Box>
+                )}
             </Box>
             <Box sx={{ padding: '20px', position: 'sticky', bottom: '10px' }}>
                 <ButtonPlay
