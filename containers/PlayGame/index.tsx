0ec3490fc303693f83669a50592a1ce8665dbecf
ebennaga <freddy.eben@gmail.com>
2022-10-10 14:33:55 +0700

    add

--- containers/PlayGame/index.tsx
+++ containers/PlayGame/index.tsx
@@ -8,7 +8,7 @@ const PlayGameContainer = () => {
                 // src='https://verdantegames.com/Hopup/'
                 src='https://aesthetic-kleicha-2fce21.netlify.app/'
                 style={{ position: 'absolute', top: 0, left: 0, bottom: 0, right: 0, width: '100%', height: '100%', border: 'none' }}
-                title='Prize Play'
+                title='Prize Play || HopUp'
             />
         </Box>
     );
