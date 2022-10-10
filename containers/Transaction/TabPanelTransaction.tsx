3e682b8bd6367c32c8a72d8822dc7d1f85ae1cba
faryadicka <faryadicka98@gmail.com>
2022-10-10 16:07:33 +0700

    add tab for transaction page

---
+++ containers/Transaction/TabPanelTransaction.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+interface TabPanelTransactionProps {
+    children?: React.ReactNode;
+    index: number;
+    value: number;
+}
+
+const TabPanelTransaction: React.FC<TabPanelTransactionProps> = ({ children, value, index, ...other }) => {
+    return (
+        <div role='tabpanel' hidden={value !== index} id={`simple-tabpanel-${index}`} aria-labelledby={`simple-tab-${index}`} {...other}>
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Box>{children}</Box>
+                </Box>
+            )}
+        </div>
+    );
+};
+
+export default TabPanelTransaction;
