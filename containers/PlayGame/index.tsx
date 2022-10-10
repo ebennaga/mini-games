a93cdf9747dbbd7b0211b4c47e74b494caf64c5b
ebennaga <freddy.eben@gmail.com>
2022-10-10 13:09:46 +0700

    adjust iframe

--- containers/PlayGame/index.tsx
+++ containers/PlayGame/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const PlayGameContainer = () => {
-    return <iframe src='https://verdantegames.com/Hopup/' width='100%' height='100%' title='W3Schools Free Online Web Tutorials' />;
+    return (
+        <iframe
+            src='https://verdantegames.com/Hopup/'
+            style={{ position: 'absolute', top: 0, left: 0, bottom: 0, right: 0, width: '100%', height: '100%', border: 'none' }}
+            title='Prize Play'
+            allowFullScreen
+        />
+    );
 };
 
 export default PlayGameContainer;
