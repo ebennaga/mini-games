108b5cdf8800d0fc6d4e7df6220ea92949900c60
faryadicka <faryadicka98@gmail.com>
2022-10-11 10:22:07 +0700

    fix payment confirmation page

--- containers/PaymentConfirmation/index.tsx
+++ containers/PaymentConfirmation/index.tsx
@@ -1,11 +1,48 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Checkbox, FormControlLabel } from '@mui/material';
+import Button from 'components/Button/Index';
 import Header from 'components/Header';
 import NavigationCard from 'components/NavigationCard';
 import React from 'react';
+import { Controller, useForm } from 'react-hook-form';
+
+interface CheckboxControllerProps {
+    form: any;
+    name: string;
+    onClick: any;
+    checked: any;
+    sx?: any;
+}
+
+const CheckboxController: React.FC<CheckboxControllerProps> = ({ form, name, onClick, checked, sx }) => {
+    return (
+        <Controller
+            control={form.control}
+            name={name}
+            render={({ field }) => (
+                <FormControlLabel
+                    sx={{ '& .MuiTypography-root': { fontSize: '11px' } }}
+                    label='Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, libero.'
+                    control={<Checkbox size='medium' color='secondary' onClick={onClick} checked={checked} sx={sx} {...field} />}
+                />
+            )}
+        />
+    );
+};
 
 const PaymentConfirmationContainer = () => {
+    const form = useForm({
+        mode: 'all',
+        defaultValues: {
+            check: false
+        }
+    });
+
+    const handleCheckbox = (e: any) => {
+        form.setValue('check', e.target.check);
+    };
+
     return (
-        <Box sx={{ width: '100%' }} marginBottom='500px'>
+        <Box sx={{ width: '100%' }}>
             <Box sx={{ position: 'sticky', top: '0px', backgroundColor: 'white', p: '20px', zIndex: 99 }}>
                 <Header logo='/icons/logo.svg' profilePicture='/icons/dummy/profile.png' />
             </Box>
@@ -25,7 +62,7 @@ const PaymentConfirmationContainer = () => {
                             borderRadius: '15px',
                             display: 'flex',
                             justifyContent: 'space-between',
-                            height: '150px'
+                            height: '120px'
                         }}
                     >
                         <Box sx={{ width: '50%', display: 'flex', alignItems: 'flex-end' }}>
@@ -54,7 +91,7 @@ const PaymentConfirmationContainer = () => {
                 }}
             >
                 <Box sx={{ boxSizing: 'border-box', width: '100%' }}>
-                    {[...Array(6)].map((i: any, idx: number) => (
+                    {[...Array(10)].map((i: any, idx: number) => (
                         <Box key={idx} sx={{ padding: '10px', backgroundColor: 'white', mb: '1px', cursor: 'pointer' }}>
                             <NavigationCard
                                 icon='/images/card-img.png'
@@ -66,8 +103,11 @@ const PaymentConfirmationContainer = () => {
                     ))}
                 </Box>
             </Box>
-            <Box>
-                <Typography>Test</Typography>
+            <Box sx={{ padding: '19px' }}>
+                <CheckboxController name='check' form={form} onClick={handleCheckbox} checked={form.watch('check')} />
+            </Box>
+            <Box sx={{ padding: '20px', position: 'sticky', bottom: '0px', zIndex: 99, mt: '100px' }}>
+                <Button title='Confirm to pay' backgoundColor='#A54CE5' color='white' onClick={() => {}} />
             </Box>
         </Box>
     );
