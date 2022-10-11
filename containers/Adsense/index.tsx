ff9f4125d9d161dfa3d9f83541153d8f5569c740
ebennaga <freddy.eben@gmail.com>
2022-10-11 18:00:18 +0700

    add

---
+++ containers/Adsense/index.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable no-use-before-define */
+import React from 'react';
+import addAdsense from './ads';
+
+const Adsense = () => {
+    React.useEffect(() => {
+        addAdsense();
+    }, []);
+    return (
+        // <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+        //     <div>
+        //         <div id='page-content'>
+        //             <div id='video-container'>
+        //                 <video id='video-element' playsInline muted>
+        //                     <source src='https://storage.googleapis.com/gvabox/media/samples/stock.mp4' />
+        //                 </video>
+        //                 <div id='ad-container' />
+        //             </div>
+
+        //             {/* <div id='adContainer' /> */}
+        //             <Button id='play-button'>Play</Button>
+        //         </div>
+        //     </div>
+        // </Box>
+        <div id='mainContainer'>
+            <div id='content'>
+                <video id='contentElement' playsInline muted>
+                    <source src='https://storage.googleapis.com/gvabox/media/samples/stock.mp4' />
+                </video>
+            </div>
+            <div id='adContainer' />
+        </div>
+    );
+};
+
+export default Adsense;
