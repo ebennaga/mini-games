9127f63857d299fdc38f168fd538c1ebe07d9a8c
faryadicka <faryadicka98@gmail.com>
2022-10-20 18:57:02 +0700

    play tournament

---
+++ containers/PlayTournament/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Box } from '@mui/material';
+import { useSelector } from 'react-redux';
+
+const PlayTournamentContainer = () => {
+    const [soundGame, setSoundGame] = React.useState<any>('');
+
+    const userState = useSelector((state: any) => state.webpage?.user?.user);
+
+    React.useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const soundData = localStorage.getItem('prizePlaySound');
+            if (soundData) {
+                setSoundGame(soundData);
+            } else {
+                setSoundGame('true');
+            }
+        }
+    }, []);
+
+    if (!userState && !userState) {
+        return <Box />;
+    }
+    return (
+        <Box sx={{ width: '100%' }}>
+            <iframe
+                src={`${userState?.gameUrl}?sessionIDGame=${userState.sessionGame}&token=${userState?.api_token}&userID=${userState?.id}&isSound=${soundGame}`}
+                // src={`http://192.168.0.137/swords_web/?sessionIDGame=${userState.sessionGame}&token=${userState?.api_token}&userID=${userState?.id}&isSound=${soundGame}`}
+                style={{
+                    border: '1px solid red',
+                    width: '100%',
+                    height: '100vh',
+                    padding: '0px'
+                }}
+                title='Prize Play || HopUp'
+            />
+        </Box>
+    );
+};
+
+export default PlayTournamentContainer;
