2c0009dd5843e6910f26cadcea45f4113e5bb221
rintosaputro <saputrorinto@gmail.com>
2022-10-20 16:29:40 +0700

    fix iframe src

--- containers/PlayGame/index.tsx
+++ containers/PlayGame/index.tsx
@@ -1,31 +1,32 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import { Box } from '@mui/material';
 import { useSelector } from 'react-redux';
 
 const PlayGameContainer = () => {
+    const [soundGame, setSoundGame] = React.useState<any>('');
+
     const userState = useSelector((state: any) => state.webpage?.user?.user);
 
-    if (!userState) {
+    React.useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const soundData = localStorage.getItem('prizePlaySound');
+            if (soundData) {
+                setSoundGame(soundData);
+            } else {
+                setSoundGame('true');
+            }
+        }
+    }, []);
+
+    if (!userState && !userState) {
         return <Box />;
     }
     return (
         <Box sx={{ width: '100%' }}>
             <iframe
-                src={`${userState?.gameUrl}?sessionIDGame=${userState.sessionGame}&token=${userState?.api_token}`}
-                // src={
-                //     `${gameDetail?.game_url}?=${sessionGame}` ||
-                //     `http://prizeplay-minigames.s3-website.ap-southeast-3.amazonaws.com/swords?=${sessionGame}`
-                // }
-                // src='https://minigames.prizeplay.io/swords/'
+                src={`${userState?.gameUrl}?sessionIDGame=${userState.sessionGame}&token=${userState?.api_token}&userID=${userState?.id}&isSound=${soundGame}`}
+                // src={`http://192.168.0.137/swords_web/?sessionIDGame=${userState.sessionGame}&token=${userState?.api_token}&userID=${userState?.id}&isSound=${soundGame}`}
                 style={{
-                    // border: '1px solid red',
-                    // backgroundColor: 'red',
-                    // position: 'absolute',
-                    // top: 0,
-                    // left: 0,
-                    // bottom: 0,
-                    // right: 0,
                     width: '100%',
                     height: '100vh',
                     padding: '0px'
