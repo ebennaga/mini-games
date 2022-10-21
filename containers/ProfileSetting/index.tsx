9dcf91fab144d949d4390625a123dd611089d8da
faryadicka <faryadicka98@gmail.com>
2022-10-21 10:39:09 +0700

    fix profile

--- containers/ProfileSetting/index.tsx
+++ containers/ProfileSetting/index.tsx
@@ -49,7 +49,7 @@ const ProfileSetting = () => {
     const handleDelete = () => router.push('/signup');
 
     const generalItem = [
-        { title: 'Avatar & Nickname', icon: '/icons/dummy/profile-2.png', href: '/profile/edit-profile' },
+        { title: 'Avatar & Nickname', icon: '/icons/dummy/profile.png', href: '/profile/edit-profile' },
         { title: 'Account & Address', icon: '/icons/email.svg', href: '/profile/settings/email-address' }
     ];
     const supportData = [
