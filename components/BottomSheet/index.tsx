d0545bf11bb6e9fc6415d976900c2d3556df24f4
jennifergoldwin <jennifer.goldwin@binus.ac.id>
2022-10-28 11:04:58 +0700

    contact us reported issue

--- components/BottomSheet/index.tsx
+++ components/BottomSheet/index.tsx
@@ -15,11 +15,13 @@ import {
 import CloseIcon from '@mui/icons-material/Close';
 import { makeStyles } from '@mui/styles';
 import OpenContext from 'hooks/useContext';
+import NavigationCard from 'components/NavigationCard';
 
 interface BottomSheetProps {
     items: any;
     // eslint-disable-next-line no-unused-vars
     onConfirm: (value: string) => void;
+    type: string;
 }
 const useStyles = makeStyles({
     newPosOfDialog: {
@@ -30,7 +32,7 @@ const useStyles = makeStyles({
         height: 'fit-content'
     }
 });
-const BottomSheetCustom: React.FC<BottomSheetProps> = ({ items, onConfirm }) => {
+const BottomSheetCustom: React.FC<BottomSheetProps> = ({ items, onConfirm, type }) => {
     const classes = useStyles();
 
     const [value, setValue] = useState<string>('id');
@@ -70,56 +72,70 @@ const BottomSheetCustom: React.FC<BottomSheetProps> = ({ items, onConfirm }) => 
                         fontWeight={700}
                         component='span'
                     >
-                        Choose Language
+                        {type}
                     </Typography>
                     <IconButton edge='start' color='inherit' aria-label='close' onClick={() => onConfirm(value)}>
                         <CloseIcon />
                     </IconButton>
                 </Toolbar>
             </AppBar>
-            <RadioGroup value={value} name='radio-button-group' onChange={handleChange}>
-                {items.map((list: any, idx: number) => {
-                    return (
-                        <Box key={idx} sx={{ padding: '14px' }}>
-                            <FormControlLabel
-                                sx={{ fontWeight: 700, display: 'flex', justifyContent: 'space-between' }}
-                                labelPlacement='start'
-                                value={list.value}
-                                control={
-                                    <Radio
-                                        sx={{
-                                            marginRight: '20px',
-                                            color: 'grey',
-                                            '&.Mui-checked': {
-                                                color: '#A54CE5'
-                                            }
-                                        }}
+            {type === 'Contact Us' ? (
+                <Box>
+                    {items.map((list: any, idx: number) => {
+                        return (
+                            <Box key={idx} sx={{ borderBottom: '2px solid #F4F1FF', padding: '20px 0' }}>
+                                <NavigationCard icon={list.icon} title={list.title} onClick={() => onConfirm(value)} />
+                            </Box>
+                        );
+                    })}
+                </Box>
+            ) : (
+                <Box>
+                    <RadioGroup value={value} name='radio-button-group' onChange={handleChange}>
+                        {items.map((list: any, idx: number) => {
+                            return (
+                                <Box key={idx} sx={{ padding: '14px' }}>
+                                    <FormControlLabel
+                                        sx={{ fontWeight: 700, display: 'flex', justifyContent: 'space-between' }}
+                                        labelPlacement='start'
+                                        value={list.value}
+                                        control={
+                                            <Radio
+                                                sx={{
+                                                    marginRight: '20px',
+                                                    color: 'grey',
+                                                    '&.Mui-checked': {
+                                                        color: '#A54CE5'
+                                                    }
+                                                }}
+                                            />
+                                        }
+                                        label={list.title}
                                     />
-                                }
-                                label={list.title}
-                            />
-                            <Divider />
-                        </Box>
-                    );
-                })}
-            </RadioGroup>
-            <Box sx={{ marginLeft: '14px', marginRight: '14px' }}>
-                <ButtonBase
-                    onClick={() => onConfirm(value)}
-                    sx={{ background: '#A54CE5', borderRadius: '15px', width: '100%', marginTop: '27px', marginBottom: '27px' }}
-                >
-                    <Typography
-                        component='span'
-                        fontSize='14px'
-                        paddingTop='14px'
-                        paddingBottom='14px'
-                        fontWeight={700}
-                        sx={{ color: '#ffff' }}
-                    >
-                        Confirm
-                    </Typography>
-                </ButtonBase>
-            </Box>
+                                    <Divider />
+                                </Box>
+                            );
+                        })}
+                    </RadioGroup>
+                    <Box sx={{ marginLeft: '14px', marginRight: '14px' }}>
+                        <ButtonBase
+                            onClick={() => onConfirm(value)}
+                            sx={{ background: '#A54CE5', borderRadius: '15px', width: '100%', marginTop: '27px', marginBottom: '27px' }}
+                        >
+                            <Typography
+                                component='span'
+                                fontSize='14px'
+                                paddingTop='14px'
+                                paddingBottom='14px'
+                                fontWeight={700}
+                                sx={{ color: '#ffff' }}
+                            >
+                                Confirm
+                            </Typography>
+                        </ButtonBase>
+                    </Box>
+                </Box>
+            )}
         </Dialog>
     );
 };
