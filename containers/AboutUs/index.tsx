b3ec27f7a320eeb12246e48223ae0b853166b563
ebennaga <freddy.eben@gmail.com>
2022-11-14 14:24:18 +0700

    makestore debug:false and about us

--- containers/AboutUs/index.tsx
+++ containers/AboutUs/index.tsx
@@ -27,18 +27,21 @@ const AboutUs = () => {
                         I will keep posting more important posts on my Website for all of you. Please give your support and love.'
                     /> */}
                     <Typography sx={{ mt: 1, color: '#949494', fontWeight: 400, fontSize: '14px', lineHeight: '18px' }}>
-                        prizeplay.io is a Professional games, marketplace Platform. Here we will provide you only interesting content, which
-                        you will like very much. We are dedicated to providing you the best of games, marketplace, with a focus on
-                        dependability and gaming. We are working to turn our passion for games, marketplace into a booming online website.
-                        We hope you enjoy our games, marketplace as much as we enjoy offering them to you.
+                        prizeplay.io is a professional gaming marketplace platform, featuring exciting content which can win you a variety
+                        of exclusive prizes! We are dedicated to providing you the best experience when it comes to gaming and online
+                        commerce. We are constantly working on creating the best events, and competitions for everyone to enjoy. We only
+                        employ legitimate means to ensuring a prize won is a prize earned, and we pride ourselves in being fair, and just
+                        when it comes to users utilizing our platform.
                     </Typography>
                     <Typography sx={{ mt: 1, color: '#949494', fontWeight: 400, fontSize: '14px', lineHeight: '18px' }}>
-                        I will keep posting more important posts on my Website for all of you. Please give your support and love.
+                        We sincerely hope you have a great time here, and if you have any further inquiries, please do not hesitate to
+                        contact us!
                     </Typography>
                     <Typography sx={{ mt: 1, color: '#949494', fontWeight: 400, fontSize: '14px' }}>
-                        Thanks For Visiting Our Site
+                        Updates and events will constantly be updated on this website for all to see! Thank you for your kind support, and
+                        your gratitude!
                     </Typography>
-                    <Typography sx={{ mt: 1, color: '#949494', fontWeight: 700, fontSize: '14px' }}>Have a nice play!</Typography>
+                    <Typography sx={{ mt: 1, color: '#949494', fontWeight: 700, fontSize: '14px' }}>Have Fun!!</Typography>
                 </Box>
             </Box>
             <Box sx={{ borderTop: '1px solid #E6E6E6', marginTop: '25px' }}>
