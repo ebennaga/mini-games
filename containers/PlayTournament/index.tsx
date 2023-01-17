d32302bf642c4a8895056f8e94fc9f8411f08c75
ebennaga <freddy.eben@gmail.com>
2023-01-17 13:49:42 +0700

    update adsense and version 0.4.2.3

--- containers/PlayTournament/index.tsx
+++ containers/PlayTournament/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import React from 'react';
 import { Box } from '@mui/material';
 import { useSelector } from 'react-redux';
@@ -35,7 +36,39 @@ const PlayTournamentContainer = () => {
             router.push(`${arr.join('/')}/result`);
         }
     };
-
+    React.useEffect(() => {
+        window.googletag = window.googletag || { cmd: [] };
+        googletag.cmd.push(function () {
+            // googletag
+            //     .defineSlot(
+            //         '/21622890900,22860604212/ID_prizeplay.io_res_cate_top_320x100//320x50',
+            //         [
+            //             [320, 50],
+            //             [320, 100]
+            //         ],
+            //         'div-gpt-ad-1673345092793-0'
+            //     )
+            //     // .setCollapseEmptyDiv(true)
+            //     .addService(googletag.pubads());
+            // googletag.pubads().enableSingleRequest();
+            // googletag.enableServices();
+            // googletag.display('div-gpt-ad-1673345092793-0');
+            googletag
+                .defineSlot(
+                    '/21622890900,22860604212/ID_prizeplay.io_res_cate_top_320x100//320x50',
+                    [
+                        [320, 50],
+                        [320, 100]
+                    ],
+                    'div-gpt-ad-1673345092793-0'
+                )
+                .setCollapseEmptyDiv(true)
+                .addService(googletag.pubads());
+            googletag.pubads().enableSingleRequest();
+            googletag.enableServices();
+            googletag.display('div-gpt-ad-1673345092793-0');
+        });
+    }, []);
     if (!userState && !userState) {
         return <Box />;
     }
--- pages/shops/index.tsx
+++ pages/shops/index.tsx
@@ -16,49 +16,6 @@ declare global {
 }
 
 const Page = () => {
-    React.useEffect(() => {
-        const gptAdSlots = [];
-        window.googletag = window.googletag || { cmd: [] };
-        googletag.cmd.push(function () {
-            const mapping1 = googletag
-                .sizeMapping()
-                .addSize(
-                    [780, 500],
-                    [
-                        [300, 250],
-                        [336, 280]
-                    ]
-                )
-                .addSize(
-                    [0, 0],
-                    [
-                        [300, 250],
-                        [336, 280],
-                        [300, 600]
-                    ]
-                )
-                .build();
-
-            gptAdSlots[0] = googletag
-                .defineSlot(
-                    '/21622890900,22860604212/ID_prizeplay.io_res_cate_mid3_336x280//300x250//320x100//320x50',
-                    [
-                        [300, 250],
-                        [320, 50],
-                        [336, 280],
-                        [320, 100]
-                    ],
-                    'div-gpt-ad-1673345406261-0'
-                )
-                .setCollapseEmptyDiv(true)
-                // .defineSizeMapping(mapping2)
-                .addService(googletag.pubads());
-            googletag.pubads().enableSingleRequest();
-            googletag.enableServices();
-            googletag.display('div-gpt-ad-1673345406261-0');
-        });
-    }, []);
-
     return (
         <>
             <Head>
